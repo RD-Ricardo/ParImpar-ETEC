e14f15bfe4d8d986269f52b52bad0fc701ac6814
RD <ricardodias169@gmail.com>
2021-04-10 22:44:16 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num;
+            Console.WriteLine("---------------");
+            Console.WriteLine("---Par Impar---");
+            Console.WriteLine("---------------");
+            Console.Write("Digite um  número:  ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if(num % 2 == 0){
+                Console.WriteLine("\nNúmero par");
+            }
+            else{
+                Console.WriteLine("\nNúmero Impar");
+            }
+
+        }
+    }
+}
